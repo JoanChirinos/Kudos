435a344e440d4885f9ad92b5ed64215324a709d3
JoanChirinos <jchirinos@stuy.edu>
2018-06-27 20:11:40 -0400

    .

--- submit.py
+++ submit.py
@@ -8,18 +8,16 @@ print 'Content-type: text/html\n\n'
 
 def go():
     fs = cgi.FieldStorage()
-    for i in fs.keys():
-        print i + "<br>"
-#    toWho = fs.getvalue('toWho')
-#    forWhy = fs.getvalue('whatItSays')
-#    
-#    f = open('kudos.txt', 'rU')
-#    t = f.read()
-#    f.close()
-#    
-#    x = open('kudos.txt', 'w+')
-#    x.write(t.strip() + '\n' + toWho.capitalize() + ',' + forWhy.lowercase())
-#    x.close()
-#    print t.strip() + '\n' + toWho.capitalize() + ',' + forWhy.lowercase()
+    toWho = fs.getvalue('toWho')
+    forWhy = fs.getvalue('whatItSays')
+    
+    f = open('kudos.txt', 'rU')
+    t = f.read()
+    f.close()
+    
+    x = open('kudos.txt', 'w+')
+    x.write(t.strip() + '\n' + toWho.capitalize() + ',' + forWhy.lowercase())
+    x.close()
+    print t.strip() + '\n' + toWho.capitalize() + ',' + forWhy.lowercase()
     
 go()
