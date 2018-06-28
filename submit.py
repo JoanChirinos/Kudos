e6dfbfb59672c5a2db40534be02e67b22cd9e53f
JoanChirinos <jchirinos@stuy.edu>
2018-06-27 20:07:15 -0400

    .

--- submit.py
+++ submit.py
@@ -11,11 +11,11 @@ def go():
     toWho = fs.getvalue('toWho')
     forWhy = fs.getvalue('whatItSays')
     
-    f = open('http://homer.stuy.edu/~jchirinos/Kudos/kudos.txt', 'rU')
+    f = open('kudos.txt', 'rU')
     t = f.read()
     f.close()
     
-    x = open('http://homer.stuy.edu/~jchirinos/Kudos/kudos.txt', 'w+')
+    x = open('kudos.txt', 'w+')
     x.write(t.strip() + '\n' + toWho.capitalize() + ',' + forWhy.lowercase())
     x.close()
     print t.strip() + '\n' + toWho.capitalize() + ',' + forWhy.lowercase()
