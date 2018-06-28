eaf2e550f0f29d651c63959f63cb6db0228be0a4
JoanChirinos <jchirinos@stuy.edu>
2018-06-28 06:56:12 -0400

    .

--- submit.py
+++ submit.py
@@ -27,6 +27,7 @@ def go():
         
         if len(t.strip()) == 0:
             print '<div class="md-space"></div><div class="row"><div class="col text-center" style="color: red">No Kudos have been submitted yet!</div></div>'
+            return
         
         kl = t.split('\n')
         
